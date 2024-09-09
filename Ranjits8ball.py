072c27836f34fa70149fa96084130f989dbacfa3
Stuuupidfish <166348875+Stuuupidfish@users.noreply.github.com>
2024-09-09 08:27:46 -0500

    Create Ranjits8ball.py

---
+++ Ranjits8ball.py
@@ -0,0 +1,13 @@
+import random
+
+answers = [
+  "It is certain",
+  "It is decidedly so", "Without ad oubt", "Yes definitely", "You may rely on it","As I can see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"
+]
+print("twenty ball")
+
+def ball():
+  input("Ask me question \n")
+  print(random.choice(answers))
+  ball()
+ball()`
